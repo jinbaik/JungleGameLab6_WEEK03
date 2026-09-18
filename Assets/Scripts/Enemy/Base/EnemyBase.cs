c9e3461d35f538b60ae3692a7089bc1525e0165b
백현진 <jinbaik2007@gmail.com>
2026-09-18 14:35:14 +0900

    feat: 적 관련 준비 준비

---
+++ Assets/Scripts/Enemy/Base/EnemyBase.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public abstract class EnemyBase : MonoBehaviour
+{
+    [SerializeField]
+    protected Transform target;
+
+    [SerializeField]
+    // 접근 속도
+    protected float speed = 0.01f;
+    [SerializeField]
+    // 최대 접근 거리
+    protected float maxDistance = 2;
+
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    public void SetData(Transform target)
+    {
+        this.target = target;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Arrow"))
+        {
+            OnHit(other);
+        }
+    }
+
+
+    protected abstract void Init();
+    protected abstract void OnHit(Collider other);
+    protected abstract void Movement();
+    protected abstract void OnDamage();
+
+    public virtual void OnAttackPlayer()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Enemy/Enemy_Ground_Basic.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_Ground_Basic : EnemyBase
+{
+    protected override void Movement()
+    {
+        if (Vector3.Distance(this.transform.position, target.position) >= maxDistance)
+        {
+            this.transform.Translate((target.position - this.transform.position) * Time.deltaTime * speed);
+        }
+    }
+
+    protected override void OnDamage()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    protected override void Init()
+    {
+
+    }
+
+    public void SetData(Transform target, float speed)
+    {
+        SetData(target);
+        this.speed = speed;
+    }
+
+
+    protected override void OnHit(Collider other)
+    {
+        OnDamage();
+    }
+}
