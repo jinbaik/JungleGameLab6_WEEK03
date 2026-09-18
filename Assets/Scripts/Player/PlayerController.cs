9d4705015895912d2f82612babbe320187bb34ba
백현진 <jinbaik2007@gmail.com>
2026-09-18 14:34:34 +0900

    feat: Player 움직임 전면 수정

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,7 +9,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _sensitivity = 0.5f;
     [SerializeField] private float _minPitch = -40f, _maxPitch = 70f;
     [SerializeField] private float _moveSpeed = 30f;
+    [SerializeField] private Transform _visibleTarget;
     private Vector2 _rotationValue = Vector2.zero;
+    //private Vector2 _target = Vector2.zero;
+    private Vector2 _lookDelta;
 
     private float _positionOffset = 5f;
     private float _rotationOffset = 0f;
@@ -21,6 +24,11 @@ public class PlayerController : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        HandleVisibleTarget();
     }
 
     void FixedUpdate()
@@ -35,12 +43,21 @@ public class PlayerController : MonoBehaviour
 
     private void OnLook(InputValue value)
     {
+        /*
         Vector2 inputValue = value.Get<Vector2>();
+
+        Debug.Log(inputValue);
 
         _rotationValue.x -= inputValue.y * _sensitivity;
         _rotationValue.y += inputValue.x * _sensitivity;
 
         _rotationValue.x = Mathf.Clamp(_rotationValue.x, _minPitch, _maxPitch);
+
+        _visibleTarget.transform.position -= new Vector3(inputValue.x, 0, inputValue.y) * _sensitivity ;
+        */
+
+
+        _lookDelta -= value.Get<Vector2>();
     }
 
     private void OnAttack(InputValue value)
@@ -52,10 +69,25 @@ public class PlayerController : MonoBehaviour
             StartCoroutine(ResetTransform());
         }
     }
+    
+    private void HandleVisibleTarget()
+    {
+        Vector3 movement = new Vector3(
+            _lookDelta.x,
+            0f,
+            _lookDelta.y
+        );
+
+        _visibleTarget.position +=
+            movement * _sensitivity;
+
+        _lookDelta = Vector2.zero;
+    }
 
     private void HandleRotation()
     {
-        transform.rotation = Quaternion.Euler(_rotationValue.x, _rotationValue.y, 0f);
+        //transform.rotation = Quaternion.Euler(_rotationValue.x, _rotationValue.y, 0f);
+        transform.LookAt(_visibleTarget);
     }
 
     private void HandleAcceleration()
